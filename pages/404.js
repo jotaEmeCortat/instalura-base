7b99d97d9210609d8621f3cb6a76536da1565b7b
jotaEmeCortat <jmcortat@gmail.com>
2021-02-11 14:46:10 -0300

    Modulo 01 Aula 01

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+
+export default function Page404 (){
+  return(
+    <div>
+      Página 404
+    </div>
+  )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,8 @@
+
+export default function PageSobre (){
+  return(
+    <div>
+      Sobre
+    </div>
+  )
+}
