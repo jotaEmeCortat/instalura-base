9dd06b85ce3e45ca88e01322037573a897cc48d9
jotaEmeCortat <jmcortat@gmail.com>
2021-02-12 22:33:15 -0300

    Modulo 01 Aula 04

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 /* import styled from 'styled-components'; */
 import Menu from '../src/components/commons/Menu';
+import Footer from '../src/components/Footer';
 
 
 export default function Home(){
   return (
-    <div>
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
       <Menu/>
+      <Footer/>
     </div>
   )
 };
