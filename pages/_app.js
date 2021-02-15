220dd66103dee2087e64bea531307ce7e10de03c
jotaEmeCortat <jmcortat@gmail.com>
2021-02-15 20:59:44 -0300

    Modulo 01 Aula 05

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
-import theme from '../src/assets/theme';
-import { GlobalStyle } from '../src/assets/theme/GlobalStyle';
+import theme from '../src/theme';
+import { GlobalStyle } from '../src/theme/GlobalStyle';
 import Head from 'next/head';
 
 
